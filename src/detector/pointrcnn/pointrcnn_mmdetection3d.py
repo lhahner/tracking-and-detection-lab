425d044716b652d817cf7b1bd5a408a317aa0c81
lhahner <lennart.hahner@gmail.com>
2026-05-09 23:44:46 +0200

    finalizes format for rcnn mmdetection3d and add reliable installation instructions for HPC

--- src/detector/pointrcnn/pointrcnn_mmdetection3d.py
+++ src/detector/pointrcnn/pointrcnn_mmdetection3d.py
@@ -2,24 +2,16 @@ import torch
 import sys
 import os
 import math
-
+import numpy as np
 from detector.detector import Detector
 from torch.utils.data import DataLoader
 from util.file_handler import write_output
 from pytorch3d.structures import Pointclouds
 
 if torch.cuda.is_available():
-    from mmdet3d.apis import init_model, inteference_detector
+    from mmdet3d.apis import init_model, inference_detector
 else:
-    PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
-    SRC_ROOT = os.path.join(PROJECT_ROOT, "src")
-    MMDET3D_SRC_ROOT = os.path.join(PROJECT_ROOT, "external", "mmdetection3d-cpu-only")
-    if SRC_ROOT not in sys.path:
-        sys.path.insert(0, SRC_ROOT)
-    if MMDET3D_SRC_ROOT not in sys.path:
-        sys.path.insert(0, MMDET3D_SRC_ROOT)
-    from mmdet3d.apis import init_model, inteference_detector
-
+    exit()
 from util.logging_config import LoggingConfig
 
 logging_config = LoggingConfig()
@@ -35,33 +27,26 @@ class PointRCNNmmDetections3D(Detector):
         self.checkpoint_file = ''
         self.model = init_model(self.config_file, self.checkpoint_file)
 
-    @staticmethod
-    def custom_collate(batch):
-        filtered_data = []
-        filtered_samples = []
-        for item in batch:
-            filtered_data.append(item["points"])
-            filtered_samples.append(item["sample_id"])
-        return filtered_data, filtered_samples
-
     def detect(self):
-        test_dataloader = DataLoader(dataset=self.dataset, batch_size=16,\
+        test_dataloader = DataLoader(dataset=self.dataset, batch_size=16,
                                      collate_fn=custom_collate)
         formatted_detections = []
         for point, sample in test_dataloader:
-            detections = inference_detector(self.model, point)
-            data = results.gt_instances_3d() 
-            formatted_detections = self.format_detections(sample, 
-                                                          data.instance_data[instance_data.bboxes][0],
-                                                          data.instance_data[instance_data.bboxes][1],
-                                                          data.instance_data[instance_data.bboxes][2]
-                                                          data.intsance_data[instance_data.det_scores]
-                                                          )
-        write_output(detections)
+            detections = inference_detector(self.model, point)[0].detections
+            bboxes = detections.pred_instances_3d.bboxes3d
+            scores = detections.pred_instances_3d.scores_3d
+            formatted_detections.append(self.format_detections(sample,
+                                                               bboxes.cpu().numpy()[:, :3],
+                                                               bboxes.cpu().numpy()[:, 3:5],
+                                                               bboxes.cpu().numpy()[:, 6],
+                                                               scores.cpu().numpy()
+                                                               )
+                                        )
+        write_output(formatted_detections)
         return detections
 
-    def format_detections(self, frame_index: int, xyz_centroids: torch.tensor, lwh_box: torch.tensor, 
-                          rotation_box: torch.tensor, det_scores):
+    def format_detections(self, frame_index: int, xyz_centroids: np.array, lwh_box: np.array,
+                          yaw: float, det_score: float):
         """
         The tracking system requires the following dimensions
         - x_1: Left boundary coordinate
@@ -72,13 +57,12 @@ class PointRCNNmmDetections3D(Detector):
         - z_3: Bottom Right boundary coordinate
         - score: Detection confidence score
         """
-        yaw = rotation_box[2]
-        # Rotation matrix based on yaw at corner z in local coordiantes 
+        # Rotation matrix based on yaw at corner z in local coordiantes
         rotation_matrix = torch.stack(torch.stack([torch.cos(yaw), -torch.sin(yaw), 0]),
                                       torch.stack([torch.sin(yaw), torch.cos(yaw)], 0),
                                       torch.stack([0, 0, 1])
                                       )
-        # compute per bounding each corner of every coodinate, 
+        # compute per bounding each corner of every coodinate,
         # 8 corners meaning 8 cooridnates
         local_box_corner = torch.stack(
                 [lwh_box[0]/2, lwh_box[1]/2, lwh_box[2]/2]  # corner x left front
@@ -91,6 +75,15 @@ class PointRCNNmmDetections3D(Detector):
                 [-lwh_box[0]/2, lwh_box[1]/2, -lwh_box[2]/2]  # corner y right back
         )
         # The coordiantes of the bounding box in world coordinates
-        world_bbox = xyz_centroids + rotation_matrix @ local_box_corner
-        return Pointclouds(points=list(world_bbox)).get_bounding_boxes
+        world_bbox = torch.from_numpy(xyz_centroids) + rotation_matrix @ local_box_corner
+        bboxes = Pointclouds(points=list(world_bbox)).get_bounding_boxes
+        return f"{frame_index},{bboxes.flatten()},{det_score},1,-1,-1,-1"
 
+    @staticmethod
+    def custom_collate(batch):
+        filtered_data = []
+        filtered_samples = []
+        for item in batch:
+            filtered_data.append(item["points"])
+            filtered_samples.append(item["sample_id"])
+        return filtered_data, filtered_samples
--- tests/test_pointrcnn_mmdetections3d.py
+++ tests/test_pointrcnn_mmdetections3d.py
@@ -2,7 +2,7 @@ import os
 import sys
 import unittest
 import torch
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, create_autospec
 import numpy as np
 
 TESTS_DIR = os.path.dirname(__file__)
@@ -11,12 +11,14 @@ SRC_ROOT = os.path.join(PROJECT_ROOT, "src")
 if SRC_ROOT not in sys.path:
     sys.path.insert(0, SRC_ROOT)
 
-from detector.pointrcnnmmdetections3D import PointRCNNmmDetections3D
-
 
 class TestPointRCNNmmDetections3D(unittest.TestCase):
     # TODO move to integration test
     def test_detect_and_format(self):
+        if not torch.cuda.is_available():
+            self.skipTest("These tests need GPU support")
+        
+        from detector.pointrcnnmmdetections3D import PointRCNNmmDetections3D
         pointrcnnmmdetections3D = PointRCNNmmDetections3D(
                 "./point_sample.bin",
                 "./models/point_rcnn_2x8_kitti-3d-3classes_20211208_151344.pth"
@@ -28,12 +30,29 @@ class TestPointRCNNmmDetections3D(unittest.TestCase):
         self.assertTrue(len(detections) > 0)  # Smoke test
 
     def test_format_detections(self):
+        test_detect_and_format(self):
+        if not torch.cuda.is_available():
+            self.skipTest("These tests need GPU support")
+        from detector.pointrcnnmmdetections3D import PointRCNNmmDetections3D
+        pointrcnnmmdetections3D = PointRCNNmmDetections3D(
+                "./point_sample.bin",
+                "./models/point_rcnn_2x8_kitti-3d-3classes_20211208_151344.pth"
+                )
+        # Each row is (x, y, z, x_size, y_size, z_size, yaw)
         bboxes = torch.tensor([
-                [0.4997, 0.7707, 0.0595, 0.4188],
-                [0.8101, 0.3105, 0.5123, 0.6263] 
-                ])
+            [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2],
+            ])
 
-        xyz_centroids = torch.tensor()
-        lwh_box = torch.tensor()
-        rotation_box = torch.tensor()
-        det_scroes = torch.tensor() 
+        xyz_centroids = bboxes.numpy()[:, :3]
+        lwh_box = bboxe.numpy()[:, 3:5]
+        yaw = bboxes.numpy()[:, 6]
+        det_scroes = np.array([0.78])
+
+        formatted_detections = pointrcnnmmdetections3.format_detections(
+                "00001",
+                xyz_centroids,
+                lwh_box,
+                yaw,
+                det_scroes
+        )
+        self.assertEquals(f"00001,0.2,0.2,0.2,0.2,0.2,1-1,-1,-1")
