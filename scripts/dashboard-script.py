b2adc7fc9c89ded10aa04988d4f2b3bcab0fd085
lhahner <lennart.hahner@gmail.com>
2026-07-14 15:12:15 +0200

    adjust new dashboard script with baseline

--- scripts/dashboard-script.py
+++ scripts/dashboard-script.py
@@ -10,6 +10,9 @@ dashboard_results_path = Path("inputs/dashboard_data_implemented_only.json")
 with open(dashboard_results_path, "r") as file:
     results = json.load(file)
 
+results = sorted(results,
+              key=lambda r: datetime.strptime(r["year"], "%Y-%m-%d")
+)
 connections = [
     (1, 2),
     (2, 7),
@@ -21,44 +24,67 @@ connections = [
 # Gathering the data
 mota = [float(result["mota_simple_track"]) for result in results]
 motp = [float(result["motp_simple_track"]) for result in results]
+mAP_baseline = [float(result["mAP_baseline"]) for result in results]
 mAP = [float(result["mAP"]) for result in results]
 date = [datetime.strptime(result["year"], '%Y-%m-%d').date() for result in results]
 
 # Building the plots
 fig, ax = plt.subplots(figsize=(20, 10))
 mota_mop_axis = ax.twinx()
+
+ax.set_zorder(2)
+mota_mop_axis.set_zorder(1)
+ax.patch.set_visible(False)
+
 date_positions = mdates.date2num(date) 
+
+
 
 bar_width = 25
 bar_offset = 18
 
-mota_bars = ax.bar(
+mota_bars = mota_mop_axis.bar(
         date_positions - bar_offset,
         mota,
         width=bar_width,
         alpha=0.8,
-        label="MOTA"
+        label="MOTA",
+        zorder=1
 )
-motp_bars = ax.bar(
+motp_bars = mota_mop_axis.bar(
         date_positions + bar_offset,
         motp,
         width=bar_width,
         alpha=0.8,
-        label="MOTP"
+        label="MOTP",
+        zorder=1
 )
 ax.plot(
     date,
     mAP,
-    marker="o",
-    linewidth=2.5,
-    markersize=7,
-    label="mAP"
+    label="mAP",
+    zorder=5
 )
+ax.plot(
+        date,
+        mAP_baseline,
+        label="mAP baseline",
+        zorder=5)
+
 # Annotation
 for i, result in enumerate(results):
     ax.annotate(
             result["name"],
             (datetime.strptime(result["year"], "%Y-%m-%d").date(), float(result["mAP"])),
+            textcoords="offset points",
+            xytext=(10, 5),
+            ha="left",
+            va="bottom",
+            fontsize=10
+        )
+    ax.annotate(
+            result["name"],
+            (datetime.strptime(result["year"], "%Y-%m-%d").date(), float(result["mAP_baseline"])),
             textcoords="offset points",
             xytext=(10, 5),
             ha="left",
